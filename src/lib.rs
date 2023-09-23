a4cdacd83734a2e81ad8b57ff0303909e9a53932
djedou <djedouarnaud.aman@gmail.com>
2023-09-23 22:42:52 +0200

    setup

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use binuid_app::binuid_vm::{init_trace, console::info, engine::BinuidEngine};
 #[wasm_bindgen]
 pub fn binuid_engine() {
    init_trace();
-   
    let _ = BinuidEngine::start();
    info!("Binuid Engine Ended !!!!!");
 }
