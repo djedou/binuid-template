08981c246db8b35b776c5fc3afc5cbbc6ab8b907
djedou <djedouarnaud.aman@gmail.com>
2024-05-12 13:58:18 +0200

    review

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use wasm_bindgen::prelude::*;
-use binuid_app::binuid_vm::{init_trace, console::info, engine::BinuidEngine};
+use binuid_app::binuid_vm::{init_trace, console::info, engine::BinuidEngine, vm::{binuid_instructions_table, WriteManyTable, Operand}};
 
 #[wasm_bindgen]
 pub fn binuid_engine() {
    init_trace();
-   let _ = BinuidEngine::start();
+   let constants: WriteManyTable<Operand> = WriteManyTable::new();
+   let _ = BinuidEngine::start(&constants, &binuid_instructions_table());
    info!("Binuid Engine Ended !!!!!");
 }
