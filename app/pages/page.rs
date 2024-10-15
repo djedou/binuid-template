a91744606ca00124ad87685002777a03c9a50eec
djedou <djedouarnaud.aman@gmail.com>
2024-10-15 07:37:43 +0200

    page

--- app/pages/page.rs
+++ app/pages/page.rs
@@ -1,81 +1,51 @@
-use std::{
-    logs::log_std,
-    web::{
-        subscribe::Sub, 
-        command::Cmd,
-        component::Component
-    },
-    common::{
-        default::Default,
-        from::From
-    },
-    web::events::event::Event
-};
-use app::components::{
-    cards::{Card, CardModel, CardMsg},
-    buttons::{Btn, BtnModel, BtnMsg}
+
+use binuid_std::{
+    component,
+    events::UIEvent,
+    components::{Node, Props, Cmd, PropValue}, 
+    tags::{div, text}
 };
 
 
+#[derive(Debug, Default, Clone)]
 pub struct AppModel {
-    pub card: CardModel,
-    pub increase: BtnModel,
-    pub decrease: BtnModel
+    pub lhs: u32,
+    pub rhs: u32
 }
 
+#[derive(Debug, Default, Clone)]
 pub enum AppMsg {
-    None, 
-    Increase(BtnMsg),
-    Decrease(BtnMsg),
-    Card(CardMsg)
+    #[default]
+    None,
+    Value(i8)
 }
 
-pub struct AppProps;
 
-pub struct App;
-
-impl Component for App {
-    type Model: AppModel;
-    type Msg: AppMsg;
-    type Props: AppProps;
-
-    fn view(model: Self::Model, props: Self::Props) -> Html {
-        html!(
-            <div>
-                <div>
-                    <Btn 
-                        model={model.increase}
-                        onclick={|event: Event| AppMsg::Increase(BtnMsg::OnClick)}
-                    />
-                    <Btn 
-                        model={model.decrease}
-                        onclick={|event: Event| AppMsg::Decrease(BtnMsg::OnClick)}
-                    />
-                </div>
-                <Card
-                    model={model.card}
-                />
-            </div>
-        )
-    }
-    
-    fn update(model: Self::Model, msg: Self::MSG) -> Cmd<Self::Model, Self::MSG> {
-        match msg {
-            AppMsg::Increase(increase_msg) => {
-                Cmd::<Self::Model, Self::MSG>::from(Btn::update(model.increase, increase_msg))
-            },
-            AppMsg::Decrease(decrease_msg) => {
-                Cmd::<Self::Model, Self::MSG>::from(Btn::update(model.decrease, decrease_msg))
-            },
-            AppMsg::Card(card_msg) => {
-                Cmd::<Self::Model, Self::MSG>::from(Card::update(model.card, card_msg))
-            },
-            _ => Cmd::None,
-        }
-    }
-
-    fn subscribe(model: Self::Model) -> Sub<MSG> {
-
-        Sub::None
+impl From<()> for AppModel {
+    fn from(_value: ()) -> AppModel {
+        AppModel::default()
     }
 }
+
+impl From<UIEvent> for AppMsg {
+    fn from(_value: UIEvent) -> AppMsg {
+        AppMsg::default()
+    }
+}
+
+pub fn view(
+    _model: &AppModel,
+    _props: &[(&'static str, PropValue::<AppMsg>)],
+    _children: &[Node<AppModel, AppMsg>]
+) -> Node<AppModel, AppMsg> {
+
+    Node::<AppModel, AppMsg>::default()
+}
+
+
+pub fn update(_model: &mut AppModel, _props: &mut Props<AppMsg>, _msg: &AppMsg) -> Cmd<AppMsg> {
+
+    Cmd::None
+}
+
+component!(App, app, AppModel, AppMsg, Some(view), Some(update), None, None);
